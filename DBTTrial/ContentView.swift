9087e846cb2b43e8dc390efaea0a853a2ba6d1d7
Vishnu P <vishnup@xminds.in>
2021-04-08 19:02:32 +0530

    Initial commit

---
+++ DBTTrial/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DBTTrial
+//
+//  Created by xminds on 08/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(systemName: "cloud.fill")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DBTTrial/DBTTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBTTrialApp.swift
+//  DBTTrial
+//
+//  Created by xminds on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DBTTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
